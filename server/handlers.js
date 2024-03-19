f1544ac7c480ca91bead180c94829921965ccc83
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2024-03-19 09:36:25 +0530

    used PUT, GET by ID, and DELETE Endpoints

--- server/handlers.js
+++ server/handlers.js
@@ -7,11 +7,47 @@ exports.getAllArtworks = async (req, res) => {
     }
 };
 
-exports.createArtwork = async (req, res) => {
-    
+exports.createArtwork = async (req, res) => { 
         try {
             res.send("Posted Successfully");
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     };
+
+
+exports.getArtworkById = async (req, res) => {
+    try {
+        const artwork = await Art.findById(req.params.id);
+        if (artwork == null) {
+            return res.status(404).json({ message: 'Artwork not found' });
+        }
+        res.json(artwork);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+exports.updateArtwork = async (req, res) => {
+    try {
+        const updatedArtwork = await Art.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (updatedArtwork == null) {
+            return res.status(404).json({ message: 'Artwork not found' });
+        }
+        res.json(updatedArtwork);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+exports.deleteArtwork = async (req, res) => {
+    try {
+        const deletedArtwork = await Art.findByIdAndDelete(req.params.id);
+        if (deletedArtwork == null) {
+            return res.status(404).json({ message: 'Artwork not found' });
+        }
+        res.json({ message: 'Artwork deleted' });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
