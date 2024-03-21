d47d9266a791bb24e92c034026eea8d2e5d094ac
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2024-03-21 10:22:38 +0530

    created Schema

--- server/route.js
+++ server/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const handlers = require('./handlers');
 
 router.get('/artworks', handlers.getAllArtworks);
+router.get('/artworks/:id', handlers.getArtworkById);
 router.post('/artworks', handlers.createArtwork);
 router.put('/artworks/:id', handlers.updateArtwork);
 router.delete('/artworks/:id', handlers.deleteArtwork);
