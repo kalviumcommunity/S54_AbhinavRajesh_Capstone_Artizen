5f953ee62b54d92fffbf23fe67421d0dbfcc0d7b
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2024-03-27 09:25:40 +0530

    Created Landing Page

---
+++ CLIENT/artizen/src/components/Home.jsx
@@ -0,0 +1,101 @@
+import React, { useRef, useEffect, useState } from 'react';
+import pic from '../assets/1.png';
+import man from '../assets/pic1.png'
+import logo from '../assets/logo.png';
+import '../App.css'
+import frame from '../assets/frame.png'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+const Home = () => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const parallaxRef = useRef();
+
+  useEffect(() => {
+    parallaxRef.current.scrollTo(0);
+  }, []);
+
+  const handleAnimationEnd = () => {
+    setAnimationComplete(true);
+    document.body.classList.add('parallax-active');
+    const heroImage = document.querySelector('.fade-in');
+    heroImage.style.animation = 'none';
+  };
+
+  return (
+    <div>
+      <div className='navbar'>
+        <div className='nav-icons'>
+          <button>Discover</button>
+          <button>Creators</button>
+          <button>Testimonials</button>
+        </div>
+        <div className='logo-div'>
+          <img src={logo} alt="" className='logopic' />
+        </div>
+        <div className='user-btn'>
+          <button className='login-btn'>Log In</button>
+          <button className='sign-up'>Sign Up</button>
+        </div>
+      </div>
+      <div>
+        <Parallax pages={2.85} ref={parallaxRef} className='main'>
+          <ParallaxLayer offset={0} speed={1}>
+            <h6>WHERE IMAGINATION TAKES SHAPE, VIRTUALLY</h6>
+            <h1>ARTIZEN</h1>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0.08} speed={2}>
+            <img src={pic} alt="" className={animationComplete ? "fade-in final-state" : "fade-in"} onAnimationEnd={handleAnimationEnd} />
+          </ParallaxLayer>
+          <ParallaxLayer offset={0.99} speed={1}>
+            <div className="discover-section" style={{ color: 'white' }}>
+              <div>
+                <img src={man} alt="" />
+              </div>
+              <div className='discover-section-text'>
+                <h2 className='sub-heading'>DISCOVER</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur. Commodo morbi egestas lectus ultrices sem vel. 
+                  Id sem sollicitudin mi tristique duis parturient consequat aenean ut. 
+                  Scelerisque id donec potenti dui aliquet. Enim ac habitant vivamus mattis egestas nisi sagittis. 
+                  Id dui venenatis sed
+                </p>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.2} speed={1}>
+            <div className="featured-section" style={{ color: 'white' }}>
+              <div className='featured-section-text'>
+                <h2 className='sub-heading'>FEATURED</h2>
+                <h5>Top Artworks</h5>
+              </div>
+                <div className='frame-div'>
+                  <div className='frame'></div>
+                  <div className='frame'></div>
+                  <div className='frame'></div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.999} speed={1}>
+            <div className="testimonial-section" style={{ color: 'white' }}>
+              <div className='testimonial-section-text'>
+                <h2 className='sub-heading'>TESTIMONIALS</h2>
+                <h5>From Users</h5>
+                <div className='box-div'>
+                  <div className='grey-box'></div>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={2.45} speed={1}>
+            <Box as="footer" textAlign="center" py="4" color="white" fontFamily='heverly' letterSpacing='3px'>
+              <Text>© 2024 Artizen. All Rights Reserved.</Text>
+            </Box>
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
